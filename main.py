aac0030ef685ac54614f47bb62f31549b3392f6c
Aleksey Bormatov <inferio3@gmail.com>
2025-12-03 20:03:08 +0300

    маин сделал

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import asyncio
 import os
 
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def start_command(message: types.Message):
+    await message.answer('Привет,я учусь писать код!')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
